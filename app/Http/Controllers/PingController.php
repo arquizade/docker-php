dcf1d10e43254e909761c2ceb72fbb47b55dbb7a
Dhenmark Arquiza <dhenmark.arquiza@sykescottages.co.uk>
2024-07-05 17:00:39 +0100

    Add ping endpoint

---
+++ app/Http/Controllers/PingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PingController extends Controller
+{
+    public function __invoke()
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'system running...'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/ping', PingController::class);
